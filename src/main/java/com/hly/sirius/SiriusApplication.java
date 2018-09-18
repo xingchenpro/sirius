7d7723e2f7f2f31c49bfd4c0eaf8989a0b3c14a6
黄良运 <1136513099@qq.com>
2018-09-18 19:30:28 +0800

    Initial commit

---
+++ src/main/java/com/hly/sirius/SiriusApplication.java
@@ -0,0 +1,12 @@
+package com.hly.sirius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiriusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiriusApplication.class, args);
+	}
+}
