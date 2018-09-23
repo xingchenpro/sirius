19555612b14dcc2a9da602c92aa62b1170eb9783
黄良运 <1136513099@qq.com>
2018-09-23 10:47:05 +0800

    完善细节

--- src/main/java/com/hly/sirius/AppletApplication.java
+++ src/main/java/com/hly/sirius/AppletApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAutoConfiguration//开启自动配置，逐层往下搜索加注解的类
